797b56df2e00a741e655af34b92d58afbc6e7a06
FeodorPyth <yc-user@epd7fhmsk7p79g1aclgt.auto.internal>
2024-01-31 16:02:44 +0000

    third iteration

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default_django_key')
-#'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = False
 
